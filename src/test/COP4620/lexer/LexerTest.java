479d25e2bcfc662ea1547948d58eb073e572acde
Kyle Shappley <Kyle.Shappley@gmail.com>
2019-01-29 23:22:07 -0500

    added extra testing

--- src/test/COP4620/lexer/LexerTest.java
+++ src/test/COP4620/lexer/LexerTest.java
@@ -82,4 +82,90 @@ class LexerTest {
                 arguments("WHILE", 1, new Token(ID, "WHILE"))
         );
     }
+
+    @ParameterizedTest
+    @DisplayName("IDs")
+    @MethodSource("getIdsArguments")
+    void testIds(String source, int index, Token expected) {
+        getNextToken(source, index, expected);
+    }
+
+    private static Stream<Arguments> getIdsArguments() {
+        return Stream.of(
+                arguments("djfdjfgh", 1, new Token(ID, "djfdjfgh")),
+                arguments("int hello", 2, new Token(ID, "hello")),
+                arguments("int hello=42", 2, new Token(ID, "hello")),
+                arguments("if(tacos>burritos)", 3, new Token(ID, "tacos")),
+                arguments("if(tacos>burritos)", 5, new Token(ID, "burritos"))
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("Numbers")
+    @MethodSource("getNumbersArguments")
+    void testNumbers(String source, int index, Token expected) {
+        getNextToken(source, index, expected);
+    }
+
+    private static Stream<Arguments> getNumbersArguments() {
+        return Stream.of(
+                arguments("1", 1, new Token(NUM, "1")),
+                arguments("1.0", 1, new Token(NUM, "1.0")),
+                arguments("1.000000", 1, new Token(NUM, "1.000000")),
+                arguments("11111111", 1, new Token(NUM, "11111111")),
+                arguments("11111111.00000000000", 1, new Token(NUM, "11111111.00000000000")),
+                arguments("1E5", 1, new Token(NUM, "1E5")),
+                arguments("1E+5", 1, new Token(NUM, "1E+5")),
+                arguments("1E-5", 1, new Token(NUM, "1E-5")),
+                arguments("1.0E5", 1, new Token(NUM, "1.0E5")),
+                arguments("1.0E+5", 1, new Token(NUM, "1.0E+5")),
+                arguments("1.0E-5", 1, new Token(NUM, "1.0E-5"))
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("Special Symbols")
+    @MethodSource("getSpecialSymbolsArguments")
+    void testSpecialCharacters(String source, int index, Token expected) {
+        getNextToken(source, index, expected);
+    }
+
+    private static Stream<Arguments> getSpecialSymbolsArguments() {
+        return Stream.of(
+                arguments(">=", 1, new Token(SPECIAL_SYMBOL, ">=")),
+                arguments("<=", 1, new Token(SPECIAL_SYMBOL, "<=")),
+                arguments("!=", 1, new Token(SPECIAL_SYMBOL, "!=")),
+                arguments("==", 1, new Token(SPECIAL_SYMBOL, "==")),
+                arguments("+", 1, new Token(SPECIAL_SYMBOL, "+")),
+                arguments("-", 1, new Token(SPECIAL_SYMBOL, "-")),
+                arguments("*", 1, new Token(SPECIAL_SYMBOL, "*")),
+                arguments("/", 1, new Token(SPECIAL_SYMBOL, "/")),
+                arguments("<", 1, new Token(SPECIAL_SYMBOL, "<")),
+                arguments(">", 1, new Token(SPECIAL_SYMBOL, ">")),
+                arguments("=", 1, new Token(SPECIAL_SYMBOL, "=")),
+                arguments(";", 1, new Token(SPECIAL_SYMBOL, ";")),
+                arguments(",", 1, new Token(SPECIAL_SYMBOL, ",")),
+                arguments("(", 1, new Token(SPECIAL_SYMBOL, "(")),
+                arguments(")", 1, new Token(SPECIAL_SYMBOL, ")")),
+                arguments("[", 1, new Token(SPECIAL_SYMBOL, "[")),
+                arguments("]", 1, new Token(SPECIAL_SYMBOL, "]")),
+                arguments("}", 1, new Token(SPECIAL_SYMBOL, "}")),
+                arguments("{", 1, new Token(SPECIAL_SYMBOL, "{"))
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("Comments")
+    @MethodSource("getCommentsArguments")
+    void testComments(String source, int index, Token expected) {
+        getNextToken(source, index, expected);
+    }
+
+    private static Stream<Arguments> getCommentsArguments() {
+        return Stream.of(
+                arguments("djfdjfgh//dfgdjfg", 2, null),
+                arguments("/*hello*/ world", 1, new Token(ID, "world")),
+                arguments("/*/* hello */*/ world", 1, new Token(ID, "world"))
+        );
+    }
 }
