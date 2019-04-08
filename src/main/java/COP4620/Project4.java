72a5f34eb4b6d67533571e26e56ffb0004914f6b
Kyle Shappley <Kyle.Shappley@gmail.com>
2019-04-07 22:55:35 -0400

    added P4 main

---
+++ src/main/java/COP4620/Project4.java
@@ -0,0 +1,30 @@
+package COP4620;
+
+import COP4620.codegen.CodeGenerator;
+import COP4620.codegen.Quadruple;
+import COP4620.lexer.Lexer;
+import COP4620.lexer.Token;
+import COP4620.parser.Parser;
+import COP4620.parser.SemanticAnalyzer;
+import COP4620.tree.nodes.Node;
+
+import java.util.List;
+
+public class Project4 {
+    public static void main(String[] args) {
+        Lexer lexer = new Lexer("void main(void){ int x; x=5; }");
+        Token[] tokens = lexer.getTokens();
+        Parser parser = new Parser(tokens);
+        Node root = parser.program();
+        SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer(root);
+        if (semanticAnalyzer.isValid()) {
+            CodeGenerator codeGen = new CodeGenerator(root);
+            List<Quadruple> instructions = codeGen.getInstructions();
+            int i = 0;
+            for (Quadruple e : instructions) {
+                e.setLine(++i);
+                System.out.println(e);
+            }
+        }
+    }
+}
