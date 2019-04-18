264f7f10a85f04d2ff1a1419d0d4666f62cf0a55
Kyle Shappley <Kyle.Shappley@gmail.com>
2019-04-18 04:12:56 -0400

    added project 4 main

--- src/main/java/COP4620/Project4.java
+++ src/main/java/COP4620/Project4.java
@@ -8,21 +8,41 @@ import COP4620.parser.Parser;
 import COP4620.parser.SemanticAnalyzer;
 import COP4620.tree.nodes.Node;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class Project4 {
     public static void main(String[] args) {
-        Lexer lexer = new Lexer("void main(void){ int x; x=1;}");
-        Token[] tokens = lexer.getTokens();
-        Parser parser = new Parser(tokens);
-        Node root = parser.program();
-        SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer(root);
-        if (semanticAnalyzer.isValid()) {
-            CodeGenerator codeGen = new CodeGenerator(root);
-            List<Quadruple> instructions = codeGen.getInstructions();
-            for (Quadruple e : instructions) {
-                System.out.println(e);
+        if (args.length < 1) {
+            System.out.println("Must specify input file name");
+            return;
+        }
+        String input = readFile(args[0]);
+        if (input != null) {
+            Lexer lexer = new Lexer(input);
+            Token[] tokens = lexer.getTokens();
+            Parser parser = new Parser(tokens);
+            Node root = parser.program();
+            SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer(root);
+            if (semanticAnalyzer.isValid()) {
+                CodeGenerator codeGen = new CodeGenerator(root);
+                List<Quadruple> instructions = codeGen.getInstructions();
+                for (Quadruple e : instructions) {
+                    System.out.println(e);
+                }
             }
         }
+    }
+
+    private static String readFile(String filename) {
+        try {
+            List<String> input = Files.readAllLines(Paths.get(filename));
+            return String.join("\n", input);
+        } catch (IOException e) {
+            System.out.println("Unable to read input file \"" + filename + "\"");
+        }
+        return null;
     }
 }
